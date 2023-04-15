eb1ac374894d3fbac1036597237ac0727e308cdb
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-04-15 15:35:34 +0200

    Remove unused shortcut for dap-ui

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -330,15 +330,7 @@ local default_plugins = {
     lazy = false,
     config = {
       icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
-      mappings = {
-        -- Use a table to apply multiple mappings
-        expand = { "<CR>", "<2-LeftMouse>" },
-        open = "o",
-        remove = "d",
-        edit = "e",
-        repl = "r",
-        toggle = "t",
-      },
+
       -- Expand lines larger than the window
       -- Requires >= 0.7
       expand_lines = vim.fn.has "nvim-0.7",
