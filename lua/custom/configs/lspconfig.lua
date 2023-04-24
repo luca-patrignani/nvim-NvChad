9adf7630b377593382597c3fccf5cf0b79148e0a
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-04-24 22:34:58 +0200

    Make diagnosttic messages as popups

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -12,6 +12,12 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+vim.diagnostic.config({
+  virtual_text = false
+})
 
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 -- 
 -- lspconfig.pyright.setup { blabla}
